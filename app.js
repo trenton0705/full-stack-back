73a6c99da3c6ec1160e561aea9111392902fcbec
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-07-24 16:01:00 -0600

    added read GET route

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req,res) => {
-  res.send('Hello World')
+  res.send('Hello')
 })
 
 app.use('/games', games)
--- routes/games.js
+++ routes/games.js
@@ -5,7 +5,15 @@ const knex = require('../db/knex')
 router.get('/', (req, res) => {
   knex('games').then((data) => {
     res.json(data)
-  } )
+  })
+})
+
+router.get('/:id', (req,res) => {
+  let id = req.params.id
+  knex('games').where('id', id)
+  .then((data) => {
+    res.json(data);
+  })
 })
 
 module.exports = router;
