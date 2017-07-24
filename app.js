7e67e54cb4239d42470725f28963c41663103b8d
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-07-24 13:24:11 -0600

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const games = require('./routes/games')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
