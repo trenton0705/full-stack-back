73c8efcfb417cf55224a76102a49c29ce0f88529
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-07-26 11:32:26 -0600

    added server side validation

--- routes/games.js
+++ routes/games.js
@@ -9,52 +9,82 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  let name = req.body.name
-  let publisher = req.body.publisher
-  let release = req.body.release
-  knex('games').insert({
-      name: name,
-      publisher: publisher,
-      release: release
-    }).returning('id')
-    .then((id) => {
-      res.send(id)
-    })
+  if(validate(req.body)) {
+    let name = req.body.name
+    let publisher = req.body.publisher
+    let release = req.body.release
+
+    knex('games').insert({
+        name: name,
+        publisher: publisher,
+        release: release
+      }).returning('id')
+      .then((id) => {
+        res.send(id)
+      })
+  } else {
+    res.json({message: 'Invalid User Input'})
+  }
+
 })
 
 router.get('/:id', (req, res) => {
-  let id = req.params.id
-  knex('games').where('id', id)
-    .then((data) => {
-      res.json(data);
-    })
+  if(checkId(req.params.id)) {
+    let id = req.params.id
+    knex('games').where('id', id)
+      .then((data) => {
+        res.json(data);
+      })
+  } else {
+    res.json({message: 'Invalid ID'})
+  }
 })
 
 router.put('/:id', (req, res) => {
-  let id = req.params.id
-  let name = req.body.name
-  let publisher = req.body.publisher
-  let release = req.body.release
+  if(checkId(req.params.id) && validate(req.body)) {
+    let id = req.params.id
+    let name = req.body.name
+    let publisher = req.body.publisher
+    let release = req.body.release
 
-  knex('games').where('id', id)
-    .update({
-      name: name,
-      publisher: publisher,
-      release: release
-    }).returning('*')
-    .then((data) => {
-      res.send(data)
-    })
+    knex('games').where('id', id)
+      .update({
+        name: name,
+        publisher: publisher,
+        release: release
+      }).returning('*')
+      .then((data) => {
+        res.send(data)
+      })
+  } else {
+    res.json({message: 'Invalid User Input'})
+  }
 })
 
 router.delete('/:id', (req, res) => {
-  let id = req.params.id
+  if(checkId(req.params.id)) {
+    let id = req.params.id
 
-  knex('games').where('id', id)
-  .del()
-  .then(() => {
-    res.json({message: 'Success'})
-  })
+    knex('games').where('id', id)
+    .del()
+    .then(() => {
+      res.json({message: 'Success'})
+    })
+  } else {
+    res.json({message: 'Invalid ID'})
+  }
 })
 
 module.exports = router;
+
+function validate(data) {
+  let verifyName = typeof data.name == 'string' && data.name.trim() != ''
+  let verifyPublisher = typeof data.publisher == 'string' && data.publisher.trim() != ''
+  let verifyRelease = typeof data.release == 'number'
+
+  return verifyName && verifyPublisher && verifyRelease
+}
+
+function checkId(data) {
+  return typeof data == 'number'
+}
